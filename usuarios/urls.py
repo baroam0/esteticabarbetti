5881aad9ea2ad19da272edbc094ab3ae6bcab5d8
Abel Barrios <barriosabel0@yahoo.com.ar>
2025-11-25 18:09:29 -0300

    Agrego templates y vistas para usuarios

---
+++ usuarios/urls.py
@@ -0,0 +1,12 @@
+
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('lista', views.listar_usuarios, name='listar_usuarios'),
+    path('crear/', views.crear_usuario, name='crear_usuario'),
+    path('editar/<int:user_id>/', views.editar_usuario, name='editar_usuario'),
+    path('usuarios/eliminar/<int:user_id>/', views.eliminar_usuario, name='eliminar_usuario'),
+]
+
---
+++ usuarios/views.py
@@ -0,0 +1,52 @@
+
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+
+@login_required
+def listar_usuarios(request):
+    usuarios = User.objects.all()
+    return render(request, 'usuarios/lista_usuarios.html', {'usuarios': usuarios})
+
+
+@login_required
+def crear_usuario(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "El nombre de usuario ya existe.")
+        else:
+            User.objects.create_user(username=username, email=email, password=password)
+            messages.success(request, "Usuario creado correctamente.")
+            return redirect('listar_usuarios')
+
+    return render(request, 'usuarios/crear.html')
+
+@login_required
+def editar_usuario(request, user_id):
+    usuario = get_object_or_404(User, id=user_id)
+
+    if request.method == 'POST':
+        usuario.username = request.POST.get('username')
+        usuario.email = request.POST.get('email')
+        usuario.save()
+        messages.success(request, "Usuario actualizado correctamente.")
+        return redirect('listar_usuarios')
+
+    return render(request, 'usuarios/editar.html', {'usuario': usuario})
+
+
+@login_required
+def eliminar_usuario(request, user_id):
+    usuario = get_object_or_404(User, id=user_id)
+    usuario.delete()
+    messages.success(request, "Usuario eliminado correctamente.")
+    return redirect('listar_usuarios')
+
+
+# Create your views here.
