6decd17c92692a83edf5dca01b68d095e0f8fbf9
Abel Barrios <barriosabel0@yahoo.com.ar>
2025-12-02 00:27:04 +0000

    cam,bio migrador

--- historiasclinicas/admin.py
+++ historiasclinicas/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import HistoriaClinica
+
+admin.site.register(HistoriaClinica)
+
 # Register your models here.
--- migradorhc.py
+++ migradorhc.py
@@ -22,37 +22,43 @@ def cargar_hc(archivo_txt):
         #next(f)
         for linea in f:
 
-            try:
-                datos = linea.strip().split(";")
-                paciente = Paciente.objects.get(idaccess=datos[1])
-                fecha = datos[2].split(" ")
-                fechahora = fecha[0] + " " + datos[3]
+            lineacompleta = linea.split("mariana")
 
-                fecha_obj = datetime.strptime(fechahora, "%d/%m/%Y %H:%M")
+            for l in lineacompleta:
 
-                fecha_formateada = fecha_obj.strftime("%Y-%m-%d %H:%M:%S")
+                try:
+                    datos = l.strip().split(";")
+                    #print(datos)
 
-                diagnostico = datos[5].replace("\n", "")
-                tratamiento = datos[6].replace("\n", "")
+                    paciente = Paciente.objects.get(idaccess=datos[1])
+                    fecha = datos[2].split(" ")
+                    fechahora = fecha[0] + " " + datos[3]
 
-                HistoriaClinica.objects.create(
-                    paciente = paciente,
-                    idaccess = datos[1],
-                    fecha = fecha_formateada,
-                    historia = datos[4],
-                    diagnostico = diagnostico,
-                    tratamiento = tratamiento,
-                    primeravez = False,
-                    responsable = None
-                )
+                    fecha_obj = datetime.strptime(fechahora, "%d/%m/%Y %H:%M")
 
-            except Exception as e:
-                print(datos)
-                print("Error")
-                print(str(e))
-                break
+                    fecha_formateada = fecha_obj.strftime("%Y-%m-%d %H:%M:%S")
+
+                    diagnostico = datos[5].replace("\n", "")
+                    tratamiento = datos[6].replace("\n", "")
+
+                    HistoriaClinica.objects.create(
+                        paciente = paciente,
+                        idaccess = datos[1],
+                        fecha = fecha_formateada,
+                        historia = datos[4],
+                        diagnostico = diagnostico,
+                        tratamiento = tratamiento,
+                        primeravez = False,
+                        responsable = None
+                    )
+
+                except Exception as e:
+                    print(datos)
+                    print("Error")
+                    print(str(e))
+                    break
 
             #print(f"Paciente {datos[0]} insertado correctamente.")
-        
+
 if __name__ == "__main__":
     cargar_hc("Historias2.txt")
