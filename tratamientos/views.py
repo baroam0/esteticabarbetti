10a668d05708c2b0f8ee9a4e076001b0dc4952e9
Abel Barrios <barriosabel0@protonmail.com>
2025-12-21 23:46:40 -0300

    Cambio de busqueda de tratamiento

--- tratamientos/views.py
+++ tratamientos/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.core.paginator import Paginator
+from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import TratamientoForm
@@ -24,7 +25,39 @@ def listar_tratamientos(request):
     else:
         page = 1
     resultados = paginador.get_page(page)
-    return render(request, 'tratamientos/lista_tratamientos.html', {'resultados': resultados})
+    return render(request, 'tratamientos/lista_tratamientos.html', {'results': resultados})
+
+
+@login_required
+def buscar_tratamientos(request):
+    parametro = request.GET.get('q', '')
+    page_number = request.GET.get('page', 1)
+
+    if parametro:
+        tratamientos = Tratamiento.objects.filter(
+            descripcion__icontains=parametro
+        ).order_by("descripcion")
+    else:
+        tratamientos = Tratamiento.objects.all().order_by("descripcion")
+
+    paginator = Paginator(tratamientos, 10)
+    page_obj = paginator.get_page(page_number)
+
+    data = [{
+        "id": p.pk,
+        "descripcion": p.descripcion,
+        "precio": p.precio,
+    } for p in page_obj]
+
+    return JsonResponse({
+        "results": data,
+        "page": page_obj.number,
+        "total_pages": paginator.num_pages,
+        "has_next": page_obj.has_next(),
+        "has_previous": page_obj.has_previous(),
+        "next_page": page_obj.next_page_number() if page_obj.has_next() else None,
+        "prev_page": page_obj.previous_page_number() if page_obj.has_previous() else None,
+    })
 
 
 @login_required
