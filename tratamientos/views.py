b7dd77a17c95cc055428efad3446a7035c160fe9
Abel Barrios <barriosabel0@protonmail.com>
2026-01-15 09:25:01 -0300

    Agrego correccion para tratamientos

--- tratamientos/views.py
+++ tratamientos/views.py
@@ -1,4 +1,5 @@
 
+
 from django.db import IntegrityError
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.models import User
@@ -7,9 +8,35 @@ from django.contrib import messages
 from django.core.paginator import Paginator
 from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 
 from .forms import TratamientoForm
 from .models import Tratamiento
+
+
+MESES = {
+    1: "Ene.",
+    2: "Feb.",
+    3: "Mar.",
+    4: "Abr.",
+    5: "May.",
+    6: "Jun.",
+    7: "Jul.",
+    8: "Ago.",
+    9: "Sep.",
+    10: "Oct.",
+    11: "Nov.",
+    12: "Dic.",
+}
+
+
+def formatear_fecha(fecha):
+    fecha_local = timezone.localtime(fecha)
+    dia = fecha_local.day
+    mes = MESES[fecha_local.month]
+    anio = fecha_local.year
+    hora = fecha_local.strftime("%H:%M")
+    return f"{dia} {mes} {anio} {hora}"
 
 
 @login_required
@@ -48,6 +75,9 @@ def buscar_tratamientos(request):
         "id": p.pk,
         "descripcion": p.descripcion,
         "precio": p.precio,
+        "fecha": formatear_fecha(p.fecha),
+        "usuario": p.usuario.username
+
     } for p in page_obj]
 
     return JsonResponse({
