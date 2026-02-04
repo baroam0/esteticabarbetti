fd131870a4f6ff65bd874cf7fa3ab8027c1ab32e
Abel Barrios <barriosabel0@protonmail.com>
2026-02-04 02:00:30 -0300

    migrtador prueba

--- migradorpaciente.py
+++ migradorpaciente.py
@@ -1,5 +1,6 @@
 
 import datetime
+import difflib
 import time
 import os
 import django
@@ -20,59 +21,67 @@ def limpiar(valor):
 def cargar_pacientes(archivo_txt):
     inicio = time.time()
     pacientes = []
+    a = 0
     c = 0
     with open(archivo_txt, "r", encoding="latin-1") as f:
-        next(f)
         for linea in f:
             datos = linea.strip().split(";")
 
-            #fecha = datos[2].split(" ")
-
             existe = Paciente.objects.filter(
-                idaccess=datos[0]
+                nombre=datos[1],
+                domicilio=datos[6],
+                telefono=datos[9]
             ).exists()
 
             if existe:
-                a = 1
-            else:    
-                fechaq = datetime.datetime.strptime(limpiar(datos[2]), "%d/%m/%Y").date() if limpiar(datos[2]) else None            
-                otroextiste = Paciente.objects.filter(
+                a = a + 1
+            else:
+
+                existe = Paciente.objects.get(
                     nombre=datos[1],
                     domicilio=datos[6],
+                    telefono=datos[9]
+                )
+
+                if datos[1] == existe.nombre:
+                    print("SIPP")
+                
+                
+                
+                
+                
+                """
+                paciente=Paciente(
+                    idaccess=datos[0],
+                    nombre=datos[1],
+                    fechanacimiento=datetime.datetime.strptime(limpiar(datos[2]), "%d/%m/%Y").date() if limpiar(datos[2]) else None,
+                    sexo=datos[3],
+                    estadocivil=datos[4],
+                    numerodocumento=datos[5],
+                    domicilio=datos[6],
+                    correoelectronico=datos[7],
+                    obrasocial=datos[8],
                     telefono=datos[9],
-                    fechanacimiento=fechaq
-                ).exists()
+                    notas=datos[10],
+                    proximoturno=datos[11],
+                    aviso=datos[12],
+                ) 
+                
+                pacientes.append(paciente)
+                """
+                c = c + 1
 
-                if otroextiste:
-                    print("Pasa")
-                else:
-                    c = c + 1
-                    paciente=Paciente(
-                        idaccess=datos[0],
-                        nombre=datos[1],
-                        fechanacimiento=datetime.datetime.strptime(limpiar(datos[2]), "%d/%m/%Y").date() if limpiar(datos[2]) else None,
-                        sexo=datos[3],
-                        estadocivil=datos[4],
-                        numerodocumento=datos[5],
-                        domicilio=datos[6],
-                        correoelectronico=datos[7],
-                        obrasocial=datos[8],
-                        telefono=datos[9],
-                        notas=datos[10],
-                        proximoturno=datos[11],
-                        aviso=datos[12],
-                    ) 
-                    pacientes.append(paciente)
-
-
+    """
     try:
         Paciente.objects.bulk_create(pacientes)
     except Exception as e: 
         print("Ocurrió un error:", e)
-        
+    """ 
     fin = time.time()
     diferencia = fin - inicio
     print("Tiempo de ejecucion " + str(diferencia))        
+    print("Pacientes: " + str(c))
+    print("Excluidos: " + str(a))
 
 if __name__ == "__main__":
     cargar_pacientes("Identificacion.txt")
