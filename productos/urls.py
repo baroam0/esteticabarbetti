fd8ab7e1d09a9a091b73631b00cae5be10a28d90
Abel Barrios <barriosabel0@protonmail.com>
2026-02-25 20:34:19 -0300

    slash de urls

--- productos/urls.py
+++ productos/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('lista/', views.listar_productos, name='listar_productos'),
-    path('buscar/', views.buscar_productos, name='buscar_productos'),
+    path('buscar', views.buscar_productos, name='buscar_productos'),
     path('crear/', views.crear_producto, name='crear_producto'),
     path('editar/<int:pk>', views.editar_producto, name='editar_producto'),
     path('historial/lista/<int:pk>', views.listar_historial, name='listar_historial'),
--- tratamientos/urls.py
+++ tratamientos/urls.py
@@ -5,9 +5,9 @@ from . import views
 
 
 urlpatterns = [
-    path('lista', views.listar_tratamientos, name='listar_tratamientos'),
+    path('lista/', views.listar_tratamientos, name='listar_tratamientos'),
     path('buscar', views.buscar_tratamientos, name='buscar_tratamientos'),
     path('crear/', views.crear_tratamiento, name='crear_tratamiento'),
-    path('editar/<int:pk>/', views.editar_tratamiento, name='editar_tratamiento'),   
+    path('editar/<int:pk>', views.editar_tratamiento, name='editar_tratamiento'),   
 ]
 
