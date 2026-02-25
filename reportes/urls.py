a507b127c60fa20c7e32486e29737fc4a241eaac
Abel Barrios <barriosabel0@protonmail.com>
2026-02-25 20:02:45 -0300

    Agrego / al final de las urls de reportes

--- reportes/urls.py
+++ reportes/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cosmiatras', views.reporte_cosmiatra, name='reporte_cosmiatra'),
-    path('productos', views.reporte_productos, name='reporte_productos'),
-    path('graficotratamientos', views.grafico_tratamientos, name='grafico_tratamientos')
+    path('cosmiatras/', views.reporte_cosmiatra, name='reporte_cosmiatra'),
+    path('productos/', views.reporte_productos, name='reporte_productos'),
+    path('graficotratamientos/', views.grafico_tratamientos, name='grafico_tratamientos')
 ]
