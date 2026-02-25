6e1a39d607a360ed3b29aab9762febdead8c71ea
Abel Barrios <barriosabel0@protonmail.com>
2026-02-25 20:30:51 -0300

    slash ingresos

--- ingresos/urls.py
+++ ingresos/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('ingresos', views.ingresos_view , name='ingresos'),
+    path('ingresos/', views.ingresos_view , name='ingresos'),
 ]
