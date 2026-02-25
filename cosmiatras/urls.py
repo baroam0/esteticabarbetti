aecf8ca13f2088698ef8bd3b04ae1259fe82c85d
Abel Barrios <barriosabel0@protonmail.com>
2026-02-25 20:23:39 -0300

    Agrego slash urls

--- cosmiatras/urls.py
+++ cosmiatras/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('crear/', views.crear_cosmiatra, name='crear_cosmiatra'),
     path('editar/<int:pk>', views.editar_cosmiatra, name='editar_cosmiatra'),
 
-    path('cosmetologa/lista', views.listar_cosmetologa, name='listar_cosmetologa'),
+    path('cosmetologa/lista/', views.listar_cosmetologa, name='listar_cosmetologa'),
     path('cosmetologa/crear/', views.crear_cosmetologa, name='crear_cosmetologa'),
     path('cosmetologa/editar/<int:pk>', views.editar_cosmetologa, name='editar_cosmetologa'),
 ]
