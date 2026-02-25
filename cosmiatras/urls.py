d751ff5888160b965d121da6c4c45067cc360e96
Abel Barrios <barriosabel0@protonmail.com>
2026-02-25 20:06:28 -0300

    CAbnios url

--- cosmiatras/urls.py
+++ cosmiatras/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('lista', views.listar_cosmiatra, name='listar_cosmiatra'),
+    path('lista/', views.listar_cosmiatra, name='listar_cosmiatra'),
     path('crear/', views.crear_cosmiatra, name='crear_cosmiatra'),
     path('editar/<int:pk>', views.editar_cosmiatra, name='editar_cosmiatra'),
 
--- pacientes/urls.py
+++ pacientes/urls.py
@@ -4,12 +4,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('lista', views.listar_pacientes, name='listar_pacientes'),
-    path('buscar', views.buscar_pacientes, name='buscar_pacientes'),
+    path('lista/', views.listar_pacientes, name='listar_pacientes'),
+    path('buscar/', views.buscar_pacientes, name='buscar_pacientes'),
     path('crear/', views.crear_paciente, name='crear_pacientes'),
     path('editar/<int:pk>', views.editar_paciente, name='editar_paciente'),
     path('eliminar/<int:pk>', views.eliminar_paciente, name='eliminar_paciente'),
-    path('unificar', views.unificar_paciente, name='unificar_paciente'),
+    path('unificar/', views.unificar_paciente, name='unificar_paciente'),
     path(
         'unificarbuscar', 
         views.unificar_buscar_pacientes, 
